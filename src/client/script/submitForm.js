7509c61fff59cb6d63b6293b1f190ec6d7879461
Donya Elbanna <donyaelbanna@yahoo.com>
2021-08-22 23:10:07 +0200

    server setup

--- src/client/script/submitForm.js
+++ src/client/script/submitForm.js
@@ -1,15 +1,22 @@
 function submitForm(event) {
     event.preventDefault()
 
-    let formText = document.getElementById('form').value
-    checkURL(formText)
+    let url = document.getElementById('form').value
+    
+    // console.log("::: Form Submitted :::")
+    // console.log(url)
 
-    console.log("::: Form Submitted :::")
-    console.log(formText)
-    document.getElementById('result').innerHTML = formText
-    // fetch('http://localhost:8080/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
+    if(checkURL(url)) {
+        fetch('http://localhost:8081/test')
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('title').innerHTML = res.title
+            document.getElementById('message').innerHTML = res.message
+            document.getElementById('time').innerHTML = res.time
+    })
+    } else {
+        document.getElementById('message').innerHTML = 'no messages'
+    }
+
+    
 }
---
+++ src/server/mockAPI.js
@@ -0,0 +1,7 @@
+let json = {
+    'title': 'message title',
+    'message': 'this is a message',
+    'time': 'time now'
+}
+
+module.exports = json
